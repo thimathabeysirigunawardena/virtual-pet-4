f253618ea88326fb3a8ca0f0f1660c7b0d4c9009
thimathabeysirigunawardena <71941129+thimathabeysirigunawardena@users.noreply.github.com>
2021-01-19 22:06:46 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+//Create variables here
+
+var dog,happyDog,database,foodS,foodStock
+
+
+function preload()
+{
+  //load images here
+ 
+ 
+}
+
+function setup() {
+  createCanvas(500, 500);
+
+  dog=createSprite(200,300)
+ 
+  dog,scale=0.6
+
+
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+}
